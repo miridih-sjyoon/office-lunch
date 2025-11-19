81b6ad8705b8ea4b5b45b40e27de308283b91b45
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2025-11-19 21:59:25 +0900

    식당 추가
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

---
+++ restaurant.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+from menu import MenuSource, get_menu_from_kakao_profile, get_menu_from_kakao_post
+
+
+class Restaurant(Enum):
+    정원정_한식뷔페 = (MenuSource.KAKAO_PLUS_FRIEND_POST, '_Zlxgxhxj')
+    한신IT타워_구내식당 = (MenuSource.KAKAO_PLUS_FRIEND_PROFILE_IMAGE, '_QRALxb')
+
+    def __init__(self, menu_source: MenuSource, menu_source_id: str):
+        self.menu_source = menu_source
+        self.menu_source_id = menu_source_id
+
+    def get_menu(self):
+        if self.menu_source == MenuSource.KAKAO_PLUS_FRIEND_PROFILE_IMAGE:
+            return get_menu_from_kakao_profile(self.menu_source_id)
+        elif self.menu_source == MenuSource.KAKAO_PLUS_FRIEND_POST:
+            return get_menu_from_kakao_post(self.menu_source_id)
+        else:
+            raise Exception(f'Unknown menu source {self.menu_source}')
