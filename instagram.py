8ee2e0d2f3879f03d3603753922bac749f49da35
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2025-11-24 23:17:22 +0900

    인스타그램 클라이언트 추가
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

---
+++ instagram.py
@@ -0,0 +1,63 @@
+import os
+from dataclasses import dataclass
+
+import dacite
+import requests
+
+
+@dataclass
+class InstagramApiResponse:
+    data: InstagramProfileInfo | None
+    message: str | None
+    status: str
+
+
+@dataclass
+class InstagramProfileInfo:
+    user: User
+
+    @dataclass
+    class User:
+        edge_owner_to_timeline_media: EdgeOwnerToTimelineMedia
+
+
+@dataclass
+class EdgeOwnerToTimelineMedia:
+    count: int
+    edges: list[Edge]
+
+    @dataclass
+    class Edge:
+        node: Node
+
+        @dataclass
+        class Node:
+            id: str
+            display_url: str
+            edge_media_to_caption: EdgeMediaToCaption
+
+
+@dataclass
+class EdgeMediaToCaption:
+    edges: list[Edge]
+
+    @dataclass
+    class Edge:
+        node: Node
+
+        @dataclass
+        class Node:
+            text: str
+
+
+def get_instagram_web_profile_info(
+        username: str,
+        instagram_app_id: str = os.environ.get('INSTAGRAM_APP_ID', '936619743392459'),
+):
+    url = 'https://www.instagram.com/api/v1/users/web_profile_info/'
+    res = requests.get(url, params={
+        'username': username,
+    }, headers={
+        'X-IG-App-ID': instagram_app_id,  # useragent mismatch
+    })
+    return dacite.from_dict(InstagramApiResponse, res.json()).data
